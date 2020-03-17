48c2b530819f9c67d9400ea714cb15db713bfd40
伊文龙 <wlong.yi@gmail.com>
2020-03-17 10:28:18 +0800

    Add config and start orderer org.

--- samples/network-tps/network.sh
+++ samples/network-tps/network.sh
@@ -5,6 +5,10 @@ function networksetup() {
     ../../scripts/peer.sh configorg -f Org1.conf 
     echo "开始启动组织: Org1"
     ../../scripts/peer.sh startorg -o Org1
+    echo "开始配置组织: Orderer"
+    ../../scripts/orderer.sh configorg -f Orderer.conf 
+    echo "开始启动组织: Orderer"
+    ../../scripts/orderer.sh startorg -o Orderer
 }
 
 function networkdown() {
@@ -12,6 +16,10 @@ function networkdown() {
     ../../scripts/peer.sh stoporg -o Org1
     echo "开始清理组织: Org1"
     rm -fr Org1
+    echo "开始停止组织进程: Orderer"
+    ../../scripts/orderer.sh stoporg -o Orderer
+    echo "开始清理组织: Orderer"
+    rm -fr Orderer
     echo "网络节点已停止并清理"
 }
 
