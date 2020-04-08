3b6140ef44cead354a8d503a4baa7e9cd6e448ac
yiwenlong <wlong.yi@gmail.com>
2020-04-08 10:38:04 +0800

    Fix bring down network, add clean chaincode work directory function. Refactor log message. Use log-utils functions replace echo.
    Former-commit-id: 0e2872538babbdc7ce91c56167bd17f343fb7220

--- samples/network-tps/network.sh
+++ samples/network-tps/network.sh
@@ -20,6 +20,8 @@ DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 export FABRIC_BIN=$(cd "$DIR"/../../fabric-bin/darwin && pwd)
 export SCRIPT_PATH=$(cd "$DIR"/../../scripts && pwd)
 
+. "$SCRIPT_PATH"/utils/log-utils.sh
+
 function networksetup() {
     startPeerOrg
     startOrdererOrg
@@ -27,46 +29,51 @@ function networksetup() {
 }
 
 function startPeerOrg() {
-    echo "开始配置组织: Org1"
+    logInfo "Config organization:" Org1
     "$SCRIPT_PATH"/peer.sh configorg -f Org1.conf
-    echo "开始启动组织: Org1"
-    "$SCRIPT_PATH"/peer.sh startorg -o Org1
+    logInfo "Start organization nodes:" Org1
+    "$SCRIPT_PATH"/peer.sh startorg -o"" Org1
+    logSuccess "Organization started:" Org1
 }
 
 function startOrdererOrg() {
-    echo "开始配置组织: Orderer"
+    logInfo "Config orderer:" Orderer
     "$SCRIPT_PATH"/orderer.sh configorg -f Orderer.conf
-    echo "开始启动组织: Orderer"
+    echo "Start orderer:" Orderer
     "$SCRIPT_PATH"/orderer.sh startorg -o Orderer
+    logSuccess "Orderer started:" Orderer
 }
 
 function upChannel() {
     export FABRIC_CFG_PATH=$(cd "$DIR"/Org1/peer0 && pwd)
-    echo "开始配置 channel: mychannel"
+    logInfo "Config channel:" mychannel
     "$SCRIPT_PATH"/channel.sh config -f mychannel.conf
-    echo "开始创建 channel: mychannel"
+    logInfo "Create channel:" mychannel
     "$SCRIPT_PATH"/channel.sh create -d $(cd "$DIR"/mychannel/Org1-peer0-mychannel-conf && pwd)
-    echo "节点 peer0 加入 channel: mychannel"
+    logInfo "Join channel:" "org1.peer0 -> mychannel"
     "$SCRIPT_PATH"/channel.sh join -d $(cd "$DIR"/mychannel/Org1-peer0-mychannel-conf && pwd)
-    echo "节点 peer1 加入 channel: mychannel"
+    logInfo "Join channel:" "org1.peer1 -> mychannel"
     "$SCRIPT_PATH"/channel.sh join -d $(cd "$DIR"/mychannel/Org1-peer1-mychannel-conf && pwd)
-    echo "更新组织 Org1 在 mychannel 中的 anchor peer 节点: peer0"
+    logInfo "Update channel:" "Achor peer for mychannel -> peer0"
     "$SCRIPT_PATH"/channel.sh updateAnchorPeer -d $(cd "$DIR"/mychannel/Org1-peer0-mychannel-conf && pwd)
+    logSuccess "Channel created:" mychannel
 }
 
 function networkdown() {
-    echo "开始停止组织进程: Org1"
+    logInfo "Down organization:" Org1
     "$SCRIPT_PATH"/peer.sh stoporg -o Org1
-    echo "开始清理组织: Org1"
-    rm -fr $DIR/Org1
-    echo "开始停止组织进程: Orderer"
+    logInfo "Clean organization:" Org1
+    rm -fr "$DIR"/Org1
+    logInfo "Down orderer:" Orderer
     "$SCRIPT_PATH"/orderer.sh stoporg -o Orderer
-    echo "开始清理组织: Orderer"
+    logInfo "Clean orderer:" Orderer
     rm -fr "$DIR"/Orderer
-    echo "开始清理 channel: mychannel"
+    logInfo "Clean channel:" mychannel
     rm -fr "$DIR"/mychannel
     supervisorctl reload > /dev/null
-    echo "网络节点已停止并清理"
+    logInfo "Clean chaincode:" tps
+    rm -fr "$DIR"/chaincode-home-cc-tps
+    logSuccess "Network stoped!"
 }
 
 function usage() {
