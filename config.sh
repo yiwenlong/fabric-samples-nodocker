8cbf7bc0acd744fa55f7bbb5f6feeac308cc0929
yiwenlong <wlong.yi@gmail.com>
2020-05-13 00:04:46 +0800

    Add auto download fabric binaries from github.com/hyperledger/fabric
    Former-commit-id: 28d74c9baadb8188280ff6c66ea0ebb6ae6270ae

--- config.sh
+++ config.sh
@@ -20,3 +20,32 @@ mkdir -p "$(pwd)/build/chaincode"
 rm -fr "$(pwd)/build/chaincode/*"
 export GOBIN="$(pwd)/build/chaincode"
 go install github.com/yiwenlong/fabric-samples-nodocker/chaincode/tps
+
+function download() {
+    local BINARY_FILE=$1
+    local URL=$2
+    echo "===> Downloading: " "${URL}"
+    wget "${URL}" || rc=$?
+    tar xvzf "${BINARY_FILE}" || rc=$?
+    rm "${BINARY_FILE}"
+    if [ -n "$rc" ]; then
+        echo "==> There was an error downloading the binary file."
+        return 22
+    else
+        echo "==> Done."
+    fi
+}
+cd ./build/
+VERSION=2.0.1
+MARCH=$(uname -m)
+FABRIC_TAG=${MARCH}-${VERSION}
+ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
+BINARY_FILE=hyperledger-fabric-${ARCH}-${VERSION}.tar.gz
+echo "===> Downloading version ${FABRIC_TAG} platform specific fabric binaries"
+download "${BINARY_FILE}" "https://github.com/hyperledger/fabric/releases/download/v${VERSION}/${BINARY_FILE}"
+if [ $? -eq 22 ]; then
+    echo
+    echo "------> ${FABRIC_TAG} platform specific fabric binary is not available to download <----"
+    echo
+    exit
+fi
