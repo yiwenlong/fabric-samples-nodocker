307af49ac3dc8fec715ba8c532e8468f9751b04c
yiwenlong <wlong.yi@gmail.com>
2020-07-15 10:44:36 +0800

    Add clean network running files function.
    Former-commit-id: 02a0f5e1dc92ef1feebfa3267ed5e7fd9c5f6c32

--- samples/network-single-org/network.sh
+++ samples/network-single-org/network.sh
@@ -97,6 +97,34 @@ function installChaincode() {
   fi
 }
 
+function clean() {
+  read -r -p "Are You Sure Clean Fabric Network Running Data? [Y/n] " input
+  case $input in
+    [yY][eE][sS]|[yY])
+    for node in "peer0" "peer1"; do
+      if [ -d "Org1/$node/data" ]; then
+        rm -fr "Org1/$node/data"
+      fi
+      if [ -f "Org1/$node/FABRIC-NODOCKER-Org1-$node.log" ]; then
+        rm -f "Org1/$node/FABRIC-NODOCKER-Org1-$node.log"
+      fi
+    done
+
+    for node in "orderer0" "orderer1" "orderer2"; do
+      if [ -d "Orderer/$node/etcdraft" ]; then
+        rm -fr "Orderer/$node/etcdraft"
+      fi
+      if [ -d "Orderer/$node/file-ledger" ]; then
+        rm -fr "Orderer/$node/file-ledger"
+      fi
+      if [ -f "Orderer/$node/FABRIC-NODOCKER-Orderer-$node.log" ]; then
+        rm -f "Orderer/$node/FABRIC-NODOCKER-Orderer-$node.log"
+      fi
+    done
+    echo "Clean Done!"
+  esac
+}
+
 function down() {
   logInfo "Down organization:" Org1
   "$SCRIPT_PATH"/peer.sh stoporg -d Org1
