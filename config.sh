f2f41aeba0ff2a879b4c2707eee49bf3cdcb1bfe
yiwenlong <wlong.yi@gmail.com>
2020-08-03 23:57:15 +0800

    Modify config.sh: support download multi-platform fabric binaries.
    Former-commit-id: 302a42d241e4225011a9b797a4b83dc18ee6bb29

--- config.sh
+++ config.sh
@@ -15,48 +15,43 @@
 # limitations under the License.
 #
 
+DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+BINARIES_DIR="$DIR/binaries/"
+
+FABRIC_VERSION="2.0.1"
+
 function download() {
     local TAR_FILE=$1
     local URL=$2
     echo "===> Downloading: " "${URL}"
     curl -OL "${URL}" || rc=$?
-    tar xvzf "${TAR_FILE}" || rc=$?
-    rm "${TAR_FILE}"
-    if [ -n "$rc" ]; then
-        echo "==> There was an error downloading the binary file."
-        return 22
-    else
-        echo "==> Done."
-    fi
 }
 
-ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
+function download_fabric_binaries() {
+  local PLATFORM="$1"
+  local ARCH="$PLATFORM-amd64"
+  local FABRIC_VERSION="$2"
+  local TAR_FILE="hyperledger-fabric-${ARCH}-${FABRIC_VERSION}.tar.gz"
+  download "${TAR_FILE}" "https://github.com/hyperledger/fabric/releases/download/v${FABRIC_VERSION}/${TAR_FILE}"
+  if [ $? -eq 22 ]; then
+      echo
+      echo "------> ${ARCH} platform specific fabric binary is not available to download <----"
+      echo
+      exit
+  fi
+  tar xvzf "${TAR_FILE}" || rc=$?
+  if [ -n "$rc" ]; then
+      echo "==> There was an error downloading the binary file."
+      return 22
+  else
+      echo "==> Done."
+  fi
+  mkdir -p "$BINARIES_DIR/$PLATFORM/fabric/"
+  cp "$DIR/bin/"* "$BINARIES_DIR/$PLATFORM/fabric"
+  rm -fr "$DIR/bin/" "$DIR/config/"
+  rm -f "$TAR_FILE"
+}
 
-rm -fr "$(pwd)/build/*"
-mkdir -p "$(pwd)/build/chaincode"
-
-cd ./build/
-
-MARCH=$(uname -m)
-FABRIC_VERSION=2.0.1
-FABRIC_TAG=${MARCH}-${FABRIC_VERSION}
-TAR_FILE=hyperledger-fabric-${ARCH}-${FABRIC_VERSION}.tar.gz
-echo "===> Downloading version ${FABRIC_TAG} platform specific fabric binaries"
-download "${TAR_FILE}" "https://github.com/hyperledger/fabric/releases/download/v${FABRIC_VERSION}/${TAR_FILE}"
-if [ $? -eq 22 ]; then
-    echo
-    echo "------> ${FABRIC_TAG} platform specific fabric binary is not available to download <----"
-    echo
-    exit
-fi
-
-cd ./chaincode/
-CHAINCODE_TPS_VERSION=1.0
-CHAINCODE_TPS_BINARY_FILE=tps-${ARCH}-${CHAINCODE_TPS_VERSION}.tar.gz
-download "${CHAINCODE_TPS_BINARY_FILE}" "https://github.com/yiwenlong/chaincode-examples/releases/download/tps-${CHAINCODE_TPS_VERSION}/${CHAINCODE_TPS_BINARY_FILE}"
-if [ $? -eq 22 ]; then
-    echo
-    echo "------> ${ARCH} platform specific tps binary is not available to download <----"
-    echo
-    exit
-fi
+for platform in "linux" "darwin" "windows"; do
+  download_fabric_binaries "$platform" "2.0.1"
+done
