47b0a6a9b81fa80d2add207ecff2ca52f391c95a
yiwenlong <wlong.yi@gmail.com>
2020-08-18 10:30:24 +0800

    Refactor chaincode install script.
    Former-commit-id: ac39bb02b3b667befefde5b57355039cf95168a7

--- samples/network-single-org/chaincode-tps/install.sh
+++ samples/network-single-org/chaincode-tps/install.sh
@@ -25,49 +25,58 @@ CHANNEL_HOME=$(cd "$DIR"/../channel-mychannel/mychannel && pwd)
 # shellcheck source=utils/log-utils.sh
 . "$SCRIPT_PATH/utils/log-utils.sh"
 
-function checkSuccess() {
-  if [[ $? != 0 ]]; then
-    exit $?
-  fi
-}
-
 logInfo "Package chaincode:" tps
-"$SCRIPT_PATH"/chaincode.sh package -f cc-tps.ini
-checkSuccess
+if ! "$SCRIPT_PATH"/chaincode.sh package -f cc-tps.ini; then
+  exit $?
+fi
 
 logInfo "Install chaincode:" "tps -> org1.peer0"
-"$SCRIPT_PATH"/chaincode.sh install -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf
-checkSuccess
+if ! "$SCRIPT_PATH"/chaincode.sh install -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf; then
+  exit $?
+fi
 
 logInfo "Install chaincode:" "tps -> org1.peer0"
-"$SCRIPT_PATH"/chaincode.sh install -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer1-mychannel-conf
-checkSuccess
+if ! "$SCRIPT_PATH"/chaincode.sh install -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer1-mychannel-conf; then
+  exit $?
+fi
 
 logInfo "Approve chaincode:" "tps"
-"$SCRIPT_PATH"/chaincode.sh approve -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf
-checkSuccess
+if ! "$SCRIPT_PATH"/chaincode.sh approve -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf; then
+  exit $?
+fi
 
 logInfo "Config chaincode server boot scripts:" "tps"
-"$SCRIPT_PATH"/chaincode.sh configChaincodeServer -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf
-checkSuccess
+if ! "$SCRIPT_PATH"/chaincode.sh configChaincodeServer -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf; then
+  exit $?
+fi
 
 logInfo "Starting chaincode server:" "tps"
-./chaincode-home-cc-tps/boot.sh
-checkSuccess
+if ! ./chaincode-home-cc-tps/boot.sh; then
+  exit $?
+fi
 
 logInfo "Commit chaincode define:" "tps"
-"$SCRIPT_PATH"/chaincode.sh commit -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf
-checkSuccess
+if ! "$SCRIPT_PATH"/chaincode.sh commit -h chaincode-home-cc-tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf; then
+  exit $?
+fi
 
 logInfo "Init chaincode:" "tps"
-"$SCRIPT_PATH"/chaincode.sh invoke -i -n tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf -v '{"Args":["Init",""]}'
+if ! "$SCRIPT_PATH"/chaincode.sh invoke -i -n tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf -v '{"Args":["Init",""]}'; then
+  exit $?
+fi
 
 sleep 3
 logInfo "Invoke chaincode:" "tps"
-"$SCRIPT_PATH"/chaincode.sh invoke -n tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf -v '{"Args":["put","whoareyou","fabric-samples-nodocker"]}'
+if ! "$SCRIPT_PATH"/chaincode.sh invoke -n tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf -v '{"Args":["put","whoareyou","fabric-samples-nodocker"]}'; then
+  exit $?
+fi
 
 sleep 3
 logInfo "Query chaincode on peer0:" "tps"
-"$SCRIPT_PATH"/chaincode.sh query -n tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf -v '{"Args":["get","whoareyou"]}'
+if ! "$SCRIPT_PATH"/chaincode.sh query -n tps -c "$CHANNEL_HOME"/Org1-peer0-mychannel-conf -v '{"Args":["get","whoareyou"]}'; then
+  exit $?
+fi
 logInfo "Query chaincode on peer1:" "tps"
-"$SCRIPT_PATH"/chaincode.sh query -n tps -c "$CHANNEL_HOME"/Org1-peer1-mychannel-conf -v '{"Args":["get","whoareyou"]}'
+if ! "$SCRIPT_PATH"/chaincode.sh query -n tps -c "$CHANNEL_HOME"/Org1-peer1-mychannel-conf -v '{"Args":["get","whoareyou"]}'; then
+  exit $?
+fi
