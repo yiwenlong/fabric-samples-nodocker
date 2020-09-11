e0c9e24392b195b4fb007437acb4f2a1fc8a333c
yiwenlong <wlong.yi@gmail.com>
2020-09-11 09:26:57 +0800

    Modify: Fix fabric binaries path.
    Former-commit-id: 49c8971d689e2f13558dc61b36efd7c13e433eff

--- samples/network-single-org/channel-mychannel2/create.sh
+++ samples/network-single-org/channel-mychannel2/create.sh
@@ -16,7 +16,9 @@
 #
 
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-export FABRIC_BIN=$(cd "$DIR"/../../../binaries/darwin/fabric && pwd)
+
+arch=$(uname -s|tr '[:upper:]' '[:lower:]')
+export FABRIC_BIN=$(cd "$DIR"/../../../binaries/"$arch"/fabric && pwd)
 export SCRIPT_PATH=$(cd "$DIR"/../../../scripts && pwd)
 export FABRIC_CFG_PATH=$(cd "$DIR"/../Org1/peer0 && pwd)
 
